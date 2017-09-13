1fb5ec8a3335e96e52b26e590330fa3e8beb2735
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-09-13 11:02:34 -0700

    Fixed bug where null values killed data transformation

--- DocumentDb/Transform/LtarDataPortalCOReTransformer.cs
+++ DocumentDb/Transform/LtarDataPortalCOReTransformer.cs
@@ -24,15 +24,36 @@ namespace Nsar.Nodes.CafEcTower.DocumentDb.Transform
             {
                 // TODO: Don't hardcode!  When you have time...
                 // TODO: Unit conversions!
-                var airTemp = group.Single(m => m.Name == "TemperatureAirTsAvg").PhysicalQuantities[0].Value;
-                var windSp = group.Single(m => m.Name == "WindSpeedTsResultant").PhysicalQuantities[0].Value;
-                var windDir = group.Single(m => m.Name == "WindDirection").PhysicalQuantities[0].Value;
-                var rH = group.Single(m => m.Name == "RelativeHumidityTsAvg").PhysicalQuantities[0].Value;
-                var precip = group.Single(m => m.Name == "PrecipitationTsAccum").PhysicalQuantities[0].Value;
-                var airPress = group.Single(m => m.Name == "PressureAirTsAvg").PhysicalQuantities[0].Value;
-                var par = group.Single(m => m.Name == "ParDensityTsAvg").PhysicalQuantities[0].Value;
-                var battVolt = group.Single(m => m.Name == "BatteryVoltageTsAvg").PhysicalQuantities[0].Value;
-                var loggerTemp = group.Single(m => m.Name == "TemperaturePanelTsAvg").PhysicalQuantities[0].Value;
+                //var at = group.SingleOrDefault(m => m.Name == "TemperatureAirTsAvg");
+                //var airTemp = at?.PhysicalQuantities[0].Value;
+
+                var airTemp = group.SingleOrDefault(m => m.Name == "TemperatureAirTsAvg")?.PhysicalQuantities[0].Value;
+
+                var windSp = group.SingleOrDefault(m => m.Name == "WindSpeedTsResultant")?.PhysicalQuantities[0].Value;
+
+                var windDir = group.SingleOrDefault(m => m.Name == "WindDirection")?.PhysicalQuantities[0].Value;
+
+                var rH = group.SingleOrDefault(m => m.Name == "RelativeHumidityTsAvg")?.PhysicalQuantities[0].Value;
+
+                var precip = group.SingleOrDefault(m => m.Name == "PrecipitationTsAccum")?.PhysicalQuantities[0].Value;
+
+                var airPress = group.SingleOrDefault(m => m.Name == "PressureAirTsAvg")?.PhysicalQuantities[0].Value;
+
+                var par = group.SingleOrDefault(m => m.Name == "ParDensityTsAvg")?.PhysicalQuantities[0].Value;
+
+                var battVolt = group.SingleOrDefault(m => m.Name == "BatteryVoltageTsAvg")?.PhysicalQuantities[0].Value;
+
+                var loggerTemp = group.SingleOrDefault(m => m.Name == "TemperaturePanelTsAvg")?.PhysicalQuantities[0].Value;
+                
+                //var airTemp = group.SingleOrDefault(m => m.Name == "TemperatureAirTsAvg").PhysicalQuantities[0].Value;
+                //var windSp = group.SingleOrDefault(m => m.Name == "WindSpeedTsResultant").PhysicalQuantities[0].Value;
+                //var windDir = group.SingleOrDefault(m => m.Name == "WindDirection").PhysicalQuantities[0].Value;
+                //var rH = group.SingleOrDefault(m => m.Name == "RelativeHumidityTsAvg").PhysicalQuantities[0].Value;
+                //var precip = group.SingleOrDefault(m => m.Name == "PrecipitationTsAccum").PhysicalQuantities[0].Value;
+                //var airPress = group.SingleOrDefault(m => m.Name == "PressureAirTsAvg").PhysicalQuantities[0].Value;
+                //var par = group.SingleOrDefault(m => m.Name == "ParDensityTsAvg").PhysicalQuantities[0].Value;
+                //var battVolt = group.SingleOrDefault(m => m.Name == "BatteryVoltageTsAvg").PhysicalQuantities[0].Value;
+                //var loggerTemp = group.SingleOrDefault(m => m.Name == "TemperaturePanelTsAvg").PhysicalQuantities[0].Value;
                 //var dto = new DateTimeOffset(group.Key, new TimeSpan(utcHourOffset, 0, 0));
 
                 DateTimeOffset dtoUtc = new DateTimeOffset(group.Key, TimeSpan.Zero);
@@ -40,11 +61,11 @@ namespace Nsar.Nodes.CafEcTower.DocumentDb.Transform
 
                 // Convert units if needed
                 // m to mm
-                precip = precip * 1000;
+                if(precip != null) precip = precip * 1000;
                 // Pa to kPa
-                airPress = airPress / 1000;
+                if (airPress != null) airPress = airPress / 1000;
                 // mol/(m^2 s) to umol/(m^2 s)
-                par = par * 1000000;
+                if (par != null) par = par * 1000000;
 
                 Observation o = new Observation(
                     ltarSiteAcronym,
